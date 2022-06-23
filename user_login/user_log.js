0d54150c30c3019cf5b82abcbb2774881925fa61
vi-thavath <vithavath.yeng@gmail.com>
2022-06-23 09:14:02 +0700

    test on user_log.js

--- user_login/user_log.js
+++ user_login/user_log.js
@@ -1,3 +1,17 @@
-function hello() {
-    return console.log("hello");
-}
+// function hello() {
+//   return console.log("hello");
+// }
+
+const clock = {
+  time: 0,
+  stopWatch: function () {
+    console.log(this);
+    const self = this;
+    setInterval(function () {
+      console.log(this);
+
+      console.log(this.time++);
+    }, 1000);
+  },
+};
+clock.stopWatch();
