36ed9b3a8f0416e1f4bbd11be369c0bdc298b27b
kchantha <thasmile1@gmail.com>
2022-07-08 15:04:05 +0700

    ORF update menu

---
+++ src/menu-items/index.js
@@ -0,0 +1,10 @@
+//import other from './other';
+import applications  from './applications';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [applications]
+};
+
+export default menuItems;
